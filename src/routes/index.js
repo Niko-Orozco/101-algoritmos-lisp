a96f6a55a32fb9d8bdcaad1df82c69c24f6faeb7
nicolas.orozco@utp.edu.co <dbolanos19@gmail.com>
2020-06-24 23:45:40 -0500

    FRONT LISTO

--- src/routes/index.js
+++ src/routes/index.js
@@ -13,12 +13,14 @@ router.get('/menu', (req, res) => {
 
 router.get('/algorithm/:id', (req, res) => {
     var mod = req.params.id;
-    var enunciado="";
-    var codigo="";
-    var tema="";
-    var definicion="";
-    var linea="";
-    res.render('modules/algoritmo', {mod, enunciado, codigo, tema, definicion, linea});
+    var enunciado="Crear un Hola mundo en C";
+    var codigo="int main\n{\n\tprintf(\"Hola Mundo\");\n}";
+    var tema="Impresión por consola";
+    var definicion="Se usa la palabra reservada printf. Entre comillas dobles se coloca el mensaje a imprimir.";
+    var linea="printf(\"Hola Mundo\");";
+    var input="";
+    var output="Hola Mundo";
+    res.render('modules/algoritmo', {mod, enunciado, codigo, tema, definicion, linea, input, output});
 });
 
 module.exports = router;
